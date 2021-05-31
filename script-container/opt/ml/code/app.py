9edf8aa5c89368bd4e5af7a4f40ea2c6d924607f
Kent Regalado <mkentregalado@gmail.com>
2021-05-31 16:07:08 +0800

    Added exercise-1 dir

---
+++ script-container/opt/ml/code/app.py
@@ -0,0 +1,31 @@
+import numpy as np
+import joblib
+import json
+import sys
+from flask import Flask, request, jsonify
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+    
+# route which will accept POST requests and return our model predictions
+@app.route('/invocations', methods=['POST'])
+def prediction():
+    # load predictor models
+    lr_model = joblib.load('/opt/ml/model/lr_model.pkl')
+    
+    # prepare input data
+    content = request.json
+    payload = np.array(content.get("floor")).reshape(-1, 1)
+    
+    # perform predictions
+    lr_predict = lr_model.predict(payload)
+    prediction = str(lr_predict[0])
+    
+    return dict({'prediction': prediction})
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
